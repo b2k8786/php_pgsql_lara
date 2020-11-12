054e508f4bc215589af399be93c39e500f71fb67
Anil Samal <b2k8786@gmail.com>
2020-11-12 10:14:14 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM php:7.4-apache
+# RUN echo "ServerName localhost" >> /etc/apache2/apache2.conf
+RUN apt-get update && apt-get install -y libpq-dev && docker-php-ext-install pdo pdo_pgsql
+
+COPY src/ /var/www/html/
+RUN chmod -R 777 /var/www/html/storage/
+RUN cd /var/www/html
+RUN php artisan migrate
+EXPOSE  8080
---
+++ src/database/migrations/2020_11_04_071632_products.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('productName')->size(20);
+            $table->string('sku')->size(10);
+            $table->string('brand')->size(10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
